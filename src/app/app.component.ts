c48c3b99308784c425dee2609c10f2592c554aa5
AlexanderShishonok <asutp_tec4@tut.by>
2019-01-08 02:04:55 +0300

    tutorial end code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  title = 'Tour of Heroes';
+
+  printHello(): void {
+    console.log('Welcome to ' + this.title);
+  }
 }
